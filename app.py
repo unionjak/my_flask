465713a167282f6efebe3b298d2afdf663d0ceaa
unionjak <stgeorgecross@googlemail.com>
2020-05-02 21:05:07 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	title = "Steve Hepples Blog"
+	return render_template("index.html", title=title)
+
+@app.route('/about')
+def about():
+	names = ["john", "mary", "wes", "sally"]
+	return render_template("about.html", names=names)
+
+
+
+
+
+
+
+
