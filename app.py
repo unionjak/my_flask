f8058c1d3d559f07b8883b85b4a9ef156f0051e8
unionjak <stgeorgecross@googlemail.com>
2020-05-03 16:18:07 +0100

    further along..

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,3 +14,16 @@ def about():
     title = "About Steve Hepple"
     names = ["john", "mary", "wes", "sally"]
     return render_template("about.html", names=names, title=title)
+
+@app.route('/subscribe')
+def subscribe():
+    title = "Subscribe To Our NewsLetter"
+    return render_template("subscribe.html", title=title)
+#the code below, note the methods=post...that is because we are posting this to a form.
+@app.route('/form', methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    title = "Thank you, Kindly..."
+    return render_template("form.html", title=title, first_name=first_name, last_name=last_name, email=email)
